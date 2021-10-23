3f6371f870504acbd1234b4bdac797fc1b60332e
GustaCani <caniatigustavo@gmail.com>
2021-10-22 22:42:07 -0300

    pause

---
+++ Aventura_Alimentar/Assets/Script/Pausar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausar : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject pausePainel;
+    public string cena;
+
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseScreen();
+        }
+    }
+
+    void PauseScreen()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            Time.timeScale = 1f;
+            pausePainel.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+            pausePainel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+}
